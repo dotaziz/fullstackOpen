4563371c63a44e8acacc07c3c39decc3c3162a40
certyfreak <certifiedfreak2001@gmail.com>
2022-06-24 17:41:45 +0000

    (part 2): phone boo remove component

--- part2/phone-book/src/remove.js
+++ part2/phone-book/src/remove.js
@@ -1,6 +1,6 @@
 import PhoneBookService from './services/phonebook'
 
-const Remove = ({id, setPersons,name})=>{
+const Remove = ({id, setPersons,name,setMessage})=>{
 
     function removePerson (id,name){
         // eslint-disable-next-line no-restricted-globals
@@ -8,7 +8,27 @@ const Remove = ({id, setPersons,name})=>{
 
         if(auth){
             PhoneBookService.remove({id})
-            .then()
+            .then(()=>{
+                setMessage({
+                    error: false,
+                    success:true,
+                    message:`removed ${name}`
+                })
+                setTimeout(() => {
+                    setMessage('start')
+                },2000);
+            })
+            .catch(err=>{
+                setMessage({
+                    error:true,
+                    success:false,
+                    message:`information of ${name} has already been removed from server `,
+                })
+                setTimeout(()=>{
+                    setMessage('start')
+                },2000)
+                console.log(err)
+            })
         .then(()=>{
             PhoneBookService.getAll()
             .then(res=>{
