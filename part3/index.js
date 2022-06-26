6497a3362de13fd319b82e33fb005d4686aa3832
certyfreak <certifiedfreak2001@gmail.com>
2022-06-26 22:24:36 +0000

    add logging

--- part3/index.js
+++ part3/index.js
@@ -3,8 +3,12 @@ const morgan = require('morgan')
 const app = express()
 
 
-app.use(morgan())
 app.use(express.json())
+morgan.token('data',(req,res)=>{
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :data '))
 
 const persons = [
     { 
