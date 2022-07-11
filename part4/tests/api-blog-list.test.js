b26d4b1c440d668c83d8017d5f8bf170d0811a38
certyfreak <certifiedfreak2001@gmail.com>
2022-07-11 21:15:59 +0000

    part4(exercise:4.10):Blog list tests, step3

--- part4/tests/api-blog-list.test.js
+++ part4/tests/api-blog-list.test.js
@@ -23,6 +23,30 @@ describe('identifier is name id',()=>{
     },10000);
 });
 
+describe('post to db',()=>{
+    let blog = {
+        'title': 'writing unit tests',
+        'author': 'aziz',
+        'url':'https://localhost:4200',
+        'likes': 0,
+    };
+    test.only('check if new blog is added',async()=>{
+        const request_initial = await api.get('/api/blogs');
+
+        await api.post('/api/blogs')
+            .send(blog)
+            .expect(201);
+
+        const request = await api.get('/api/blogs');
+        console.log(request.body);
+
+        expect(request.body).toHaveLength(request_initial.body.length + 1);
+        const data = request.body[request.body.length -1];
+        delete data.id;
+        expect(data).toEqual(blog);
+    },10000);
+});
+
 afterAll(()=>{
     mongoose.connection.close();
 });
