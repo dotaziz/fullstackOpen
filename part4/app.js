2257bdc50fa37ac8a0fe8da69c6dc25ebae2aa4c
certyfreak <certifiedfreak2001@gmail.com>
2022-07-13 14:22:44 +0000

    part4(4.18): implement jwt

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const {MONGODB_URI } = require('./utils/config');
 const {info,error} = require('./utils/logger');
 const blogsRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { errorhandler } = require('./utils/middleware');
@@ -18,8 +19,11 @@ mongoose.connect(MONGODB_URI).then(()=>{
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/login',loginRouter);
 app.use('/api/blogs',blogsRouter);
 app.use('/api/users',userRouter);
+
 app.use(errorhandler);
 
 
