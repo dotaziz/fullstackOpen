030e9bf6c236f9bc3dca98537991359a76f77973
certyfreak <certifiedfreak2001@gmail.com>
2022-07-14 21:51:50 +0000

    part4(exercise:4.20): bloglist expansion, step8

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,8 +1,28 @@
+const jwt = require('jsonwebtoken')
+
 const errorhandler = (err,req,res,next)=>{
     res.status(400).send(err.message);
     next();
 };
 
+
+const tokenExtractor = (req,res,next)=>{
+    const auth = req.get('authorization');
+    if(auth && auth.toLowerCase().startsWith('bearer')){
+        const token = auth.substring(7);
+        req.token = token;
+        console.log(token);
+    }else{
+        req.token = false;
+    }
+    next();
+};
+
+// const userExtractor = (req,res,next)=>{
+
+// }
+
 module.exports = {
     errorhandler,
+    tokenExtractor,
 };
